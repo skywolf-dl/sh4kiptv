9f60af1219d82a72bba02fe720ec2d6116da6f44
blurainer <blurainer@gmail.com>
2017-06-16 19:20:51 +0800

    change

--- 4kiptv.sh
+++ 4kiptv.sh
@@ -17,12 +17,13 @@ echo 正在增加IPTV开机自启动脚本
 cd /jffs/scripts
 #删除之前的脚本
 echo 正在删除旧文件
-rm -rf nat-start*
+rm -rf wan-start* nat-start*
 #下载自启动脚本
 echo 正在下载新文件
+wget -q --no-check-certificate https://raw.githubusercontent.com/blurainer/sh4kiptv/master/wan-start
 wget -q --no-check-certificate https://raw.githubusercontent.com/blurainer/sh4kiptv/master/nat-start
 #设置权限
-chmod -R 0755 nat-start
+chmod -R 0755 wan-start nat-start
 #完成提示
 echo 成功
 #延迟运行
